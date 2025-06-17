104e084d1abf0f9a24002e045347e04e411975f4
lehoyeon <ghdus4043@gmail.com>
2025-06-17 12:53:54 +0900

    프로젝트 셋업 재 업로드

--- greenhand/src/main/java/com/grrenhand/greenhand/GreenhandApplication.java
+++ greenhand/src/main/java/com/grrenhand/greenhand/GreenhandApplication.java
@@ -1,13 +1,13 @@
-package com.grrenhand.greengand;
+package com.grrenhand.greenhand;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class GreengandApplication {
+public class GreenhandApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(GreengandApplication.class, args);
+		SpringApplication.run(GreenhandApplication.class, args);
 	}
 
 }
--- greenhand/src/test/java/com/grrenhand/greenhand/GreenhandApplicationTests.java
+++ greenhand/src/test/java/com/grrenhand/greenhand/GreenhandApplicationTests.java
@@ -1,10 +1,10 @@
-package com.grrenhand.greengand;
+package com.grrenhand.greenhand;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class GreengandApplicationTests {
+class GreenhandApplicationTests {
 
 	@Test
 	void contextLoads() {
