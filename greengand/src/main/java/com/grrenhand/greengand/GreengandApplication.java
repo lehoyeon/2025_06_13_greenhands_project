c74f9825d057494c8d0a81292749eb6d59a96113
lehoyeon <ghdus4043@gmail.com>
2025-06-17 11:59:45 +0900

    프로젝트 셋업

---
+++ greengand/src/main/java/com/grrenhand/greengand/GreengandApplication.java
@@ -0,0 +1,13 @@
+package com.grrenhand.greengand;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GreengandApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GreengandApplication.class, args);
+	}
+
+}
---
+++ greengand/src/test/java/com/grrenhand/greengand/GreengandApplicationTests.java
@@ -0,0 +1,13 @@
+package com.grrenhand.greengand;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GreengandApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
